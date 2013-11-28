7c69ff06bd530320ccd5eef7efc3ca7c3770be34
Wolfgang Klenk <wklenk@users.noreply.github.com>
2013-11-28 13:25:38 +0100

    Update Gpio.cpp
    Added comments, unified error handling.

--- src/Gpio.cpp
+++ src/Gpio.cpp
@@ -37,35 +37,55 @@ Gpio::~Gpio() {
 	this->unexportPin();
 }
 
+/**
+ * Ask the kernel to export control of a GPIO to userspace by writing its number to file
+ * /sys/class/gpio/export.
+ */
 void Gpio::exportPin() {
 	static const char* fn = "/sys/class/gpio/export";
 	int fd = open(fn, O_WRONLY);
 	if(fd < 0) {
 		perror(fn);
-		abort();
+		exit(1);
 	}
 
 	int rc = write(fd, this->pin, strlen(this->pin));
 	if(rc != strlen(pin)) {
-		fprintf(stderr, "write export");
+		perror("write export"));
 		exit(1);
 	}
 
 	close(fd);
 }
 
+/**
+ * Reverses the effect of exporting a GPIO to userspace by writing its number to file
+ * /sys/class/gpio/unexport.
+ */
 void Gpio::unexportPin() {
 	static const char* fn = "/sys/class/gpio/unexport";
 	int fd = open(fn, O_WRONLY);
 	if(fd < 0) {
-		fprintf(stderr, fn);
+		perror(fn);
 		exit(1);
 	}
 
-	write(fd, this->pin, strlen(this->pin));
+	int rc = write(fd, this->pin, strlen(this->pin));
+	if(rc != strlen(pin)) {
+		perror("write unexport"));
+		exit(1);
+	}
+	
 	close(fd);
 }
 
+/**
+ * Sets the direction of a GPIO pin.
+ * Reads as either "in" or "out".  This value may normally be written.  
+ * Writing as "out" defaults to initializing the value as low.  
+ * To ensure glitch free operation, values "low" and "high" may be written to
+ * configure the GPIO as an output with that initial value.
+ */
 void Gpio::setPinDirection(const char* direction) {
 
 	const int BUFSIZE = 64;
@@ -73,19 +93,24 @@ void Gpio::setPinDirection(const char* direction) {
 	snprintf(fn, BUFSIZE, "/sys/class/gpio/gpio%s/direction", this->pin);
 	int fd = open(fn, O_WRONLY);
 	if(fd < 0)  {
-		fprintf(stderr, fn);
+		perror(fn);
 		exit(1);
 	}
 
 	int rc = write(fd, direction, strlen(direction));
 	if(rc != strlen(direction)) {
-		fprintf(stderr, "write direction");
+		perror("write direction");
 		exit(1);
 	}
 
 	close(fd);
 }
 
+/**
+ * Sets the signal edge(s) that will make poll(2) on the "value" file return.
+ * Reads as either "none", "rising", "falling", or "both". 
+ * This file exists only if the pin can be configured as an interrupt generating input pin.
+ */
 void Gpio::setPinEdge(const char* edge) {
 
 	const int BUFSIZE = 64;
@@ -93,19 +118,33 @@ void Gpio::setPinEdge(const char* edge) {
 	snprintf(fn, BUFSIZE, "/sys/class/gpio/gpio%s/edge", this->pin);
 	int fd = open(fn, O_WRONLY);
 	if(fd < 0)  {
-		fprintf(stderr, fn);
+		perror(fn);
 		exit(1);
 	}
 
 	int rc = write(fd, edge, strlen(edge));
 	if(rc != strlen(edge)) {
-		fprintf(stderr, "write edge");
+		perror("write edge");
 		exit(1);
 	}
 
 	close(fd);
 }
 
+/**
+ * Read the value of a GPIO pin.
+ * Returns either 0 (low) or 1 (high).  
+ * 
+ * If the pin can be configured as interrupt-generating interrupt
+ * and if it has been configured to generate interrupts (see the
+ * description of "edge"), you can poll(2) on that file and
+ * poll(2) will return whenever the interrupt was triggered. If
+ * you use poll(2), set the events POLLPRI and POLLERR. If you
+ * use select(2), set the file descriptor in exceptfds. After
+ * poll(2) returns, either lseek(2) to the beginning of the sysfs
+ * file and read the new value or close the file and re-open it
+ * to read the value.
+ */
 void Gpio::getPinValue(void* value, size_t nbytes) {
 
 	const int BUFSIZE = 64;
@@ -113,20 +152,28 @@ void Gpio::getPinValue(void* value, size_t nbytes) {
 	snprintf(fn, BUFSIZE, "/sys/class/gpio/gpio%s/value", this->pin);
 	int fd = open(fn, O_RDONLY);
 	if(fd < 0)  {
-		fprintf(stderr, fn);
+		perror(fn);
 		exit(1);
 	}
 
 
 	int rc = read(fd, value, nbytes);
 	if(rc < 0) {
-		fprintf(stderr, "read");
+		perror("read");
 		exit(1);
 	}
 
 	close(fd);
 }
 
+/**
+ * If the GPIO pin was configured to generate interrupts (see the
+ * description of "edge"), you can use this method to wait for the edge
+ * condition to happen.
+ * 
+ * Method will return with 0 if edge condition did not happen within the
+ * specified timeout.
+ */
 int Gpio::waitForPinValueChange(int timout_millis) {
 
 	const int BUFSIZE = 64;
@@ -134,21 +181,25 @@ int Gpio::waitForPinValueChange(int timout_millis) {
 	snprintf(fn, BUFSIZE, "/sys/class/gpio/gpio%s/value", this->pin);
 	int fd = open(fn, O_RDONLY);
 	if(fd < 0)  {
-		fprintf(stderr, fn);
+		perror(fn);
 		exit(1);
 	}
 
 	// Dummy read to clear the interrupt.
 	uint8_t c;
-	read (fd, &c, 1);
+	int rc = read (fd, &c, 1);
+	if (rc < 0) {
+		perror("read");
+		exit(1);
+	}
 
 	struct pollfd pl;
 	pl.fd = fd;
 	pl.events = POLLPRI | POLLERR;
 
-	int rc = poll(&pl, 1, timout_millis);
+	rc = poll(&pl, 1, timout_millis);
 	if(rc < 0) {
-		fprintf(stderr, "poll");
+		perror("poll");
 		exit(1);
 	}
 
